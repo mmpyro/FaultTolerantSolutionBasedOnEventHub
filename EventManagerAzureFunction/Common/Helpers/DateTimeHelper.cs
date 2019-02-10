ea1b5781aacb9cd332daf20a25be7a716e91504d
michal.marszalek@elmodis.com <michal.marszalek@elmodis.com>
2019-02-10 19:21:25 +0100

    Changes in PoisonMessages

---
+++ EventManagerAzureFunction/Common/Helpers/DateTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static long ToEpochTimestamp(this DateTime date)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64((date - epoch).TotalMilliseconds);
+        }
+    }
+}
